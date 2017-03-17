7406fc4914305753d18dc8a524d9f3f1b0a7e6e7
myreadings1 <myreadings1@users.noreply.github.com>
2017-03-17 11:28:23 -0400

    Add files via upload

---
+++ USRP-GNURadio/bt_tx_1.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Bt Tx 1
+# Generated: Thu Mar 16 20:02:27 2017
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import time
+import wx
+
+
+class bt_tx_1(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Bt Tx 1")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 2
+        self.samp_rate_0 = samp_rate_0 = 1e6
+        self.mod_index = mod_index = 1/1.5
+        self.freq = freq = 2404e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(sps)
+        self.uhd_usrp_sink_0.set_center_freq(freq, 0)
+        self.uhd_usrp_sink_0.set_gain(45, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.digital_gfsk_mod_0 = digital.gfsk_mod(
+        	samples_per_symbol=sps,
+        	sensitivity=(3.14*mod_index)/sps,
+        	bt=0.32,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate_0,True)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.7, ))
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "sync2", True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.uhd_usrp_sink_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.digital_gfsk_mod_0, 0))    
+        self.connect((self.digital_gfsk_mod_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.uhd_usrp_sink_0.set_samp_rate(self.sps)
+
+    def get_samp_rate_0(self):
+        return self.samp_rate_0
+
+    def set_samp_rate_0(self, samp_rate_0):
+        self.samp_rate_0 = samp_rate_0
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate_0)
+
+    def get_mod_index(self):
+        return self.mod_index
+
+    def set_mod_index(self, mod_index):
+        self.mod_index = mod_index
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
+
+
+def main(top_block_cls=bt_tx_1, options=None):
+
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+
+if __name__ == '__main__':
+    main()
